5de3af9d1a5da1598dce1c0617fa110c33acfd2e
Danny <113941896+coolhuip@users.noreply.github.com>
2023-08-12 17:10:03 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pytoolz',
+    name='pietoolz',
     version='0.0.1',
     packages=find_packages(),
     install_requires=[],
     author='coolhuip',
     author_email='cool.huip@example.com',
-    description='PyToolz is a package houseing useful classes, methods, and functions designed to streamline the development process.',
+    description='PieToolz is a package houseing useful classes, methods, and functions designed to streamline the development process.',
 )
