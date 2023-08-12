cd1c4e4bd75d4e749076c0b979d40e2e28107ee8
Danny <cool.huip@gmail.com>
2023-08-12 16:59:54 -0400

    Prepare to create Python package: PyToolz

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pytoolz',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='coolhuip',
+    author_email='cool.huip@example.com',
+    description='PyToolz is a package houseing useful classes, methods, and functions designed to streamline the development process.',
+)
