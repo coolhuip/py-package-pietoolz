27b9f2cd72a43bbeea99c5af9577c0935764bc37
Danny <cool.huip@gmail.com>
2023-08-12 22:32:50 -0400

    Update PieToolz version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pietoolz',
-    version='0.0.2',  # Update the version ONLY when you publish a new release.
+    version='0.0.3',  # Update the version ONLY when you publish a new release.
     packages=find_packages(),
     install_requires=[],
     author='coolhuip',
