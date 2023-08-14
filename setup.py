ca437c09c26368ff464e3cb7480ea290df129cd7
Danny <cool.huip@gmail.com>
2023-08-14 01:12:25 -0400

    Publish v0.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pietoolz',
-    version='0.0.4',  # Update the version ONLY when you publish a new release.
+    version='0.0.5',  # Update the version ONLY when you publish a new release.
     packages=find_packages(),
     install_requires=[],
     author='coolhuip',
