95877ce8477fb830875966d89aafbbace9b18708
Danny <cool.huip@gmail.com>
2023-08-16 22:56:49 -0400

    🚀 v0.0.9__ready_for_takeoff__

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pietoolz',
-    version='0.0.8',  # Update the version ONLY when you publish a new release.
+    version='0.0.9',  # Update the version ONLY when you publish a new release.
     packages=find_packages(),
     install_requires=[],
     author='coolhuip',
