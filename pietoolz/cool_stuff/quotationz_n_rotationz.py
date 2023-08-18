3c45be21d53baeebac385ca1f7dd293f1beb26db
Danny <cool.huip@gmail.com>
2023-08-17 23:35:30 -0400

    Update words of humanity

--- pietoolz/cool_stuff/quotationz_n_rotationz.py
+++ pietoolz/cool_stuff/quotationz_n_rotationz.py
@@ -1,7 +1,8 @@
 """
-Words of Hip-Hop.
+Words.
 """
 gimme_sum_hiphop = \
     {'Snoop Dogg': "To have money, you gotta have heart.",
      'Curtis "50 Cent" Jackson': "Don't let a number take root inside your head.",
+     'Jordan B. Peterson': "In order for a tree to reach Heaven, its roots must first reach Hell.",
     }
